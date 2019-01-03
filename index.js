b02d99442e106b7e78b276c198689888c4d8c390
fenix <a.sydorenko87@gmail.com>
2019-01-03 20:56:42 +0200

    #chore #doc ([test], index.js, package.json) minor code chore, version update

--- index.js
+++ index.js
@@ -3,81 +3,104 @@
 module.exports = {
   uuid4,
   uuid4Async,
+  uuid4AsyncClear,
   uuid4Clear
 }
+
+/**
+ * @returns {string}
+ * */
 
 function uuid4 () {
   let uuid = ''
 
   let i = 0
   while (i < 32) {
-    switch (i) {
-      case 12 :
-        uuid += '-4'
-        break
-      case 16 :
-        uuid += '-' + (Math.random() * 4 | 8).toString(16)
-        break
-      case 8 :
-      case 20 :
-        uuid += '-' + Math.trunc(Math.random() * 16).toString(16)
-
-        break
-      default:
-        uuid += Math.trunc(Math.random() * 16).toString(16)
-    }
-    i++
+    uuid += standardCase(i++)
   }
 
   return uuid
 }
+
+/**
+ * @returns {string}
+ * */
 
 function uuid4Clear () {
   let uuid = ''
 
   let i = 0
   while (i < 32) {
-    switch (i) {
-      case 12 :
-        uuid += '4'
-        break
-      case 8 :
-      case 16 :
-        uuid += (Math.random() * 4 | 8).toString(16)
-        break
-      default:
-        uuid += Math.trunc(Math.random() * 16).toString(16)
-    }
-    i++
+    uuid += clearCase(i++)
   }
 
   return uuid
 }
 
+/**
+ * @returns {object} <Promise> => {string}
+ * */
+
 function uuid4Async () {
   return new Promise((resolve) => {
-    iterator('', 0, resolve)
+    iterator(standardCase, '', 0, resolve)
   })
 }
 
-function iterator (uuid, i, cb) {
-  setImmediate(() => {
-    switch (i) {
-      case 12 :
-        uuid += '-4'
-        break
-      case 16 :
-        uuid += '-' + (Math.random() * 4 | 8).toString(16)
-        break
-      case 8 :
-      case 20 :
-        uuid += '-' + Math.trunc(Math.random() * 16).toString(16)
-        break
-      case 32 :
-        return cb(uuid)
-      default:
-        uuid += Math.trunc(Math.random() * 16).toString(16)
-    }
-    iterator(uuid, ++i, cb)
+/**
+ * @returns {object} <Promise> => {string}
+ * */
+
+function uuid4AsyncClear () {
+  return new Promise((resolve) => {
+    iterator(clearCase, '', 0, resolve)
   })
+}
+
+/**
+ * @returns {void}
+ * @callback {string}
+ * */
+
+function iterator (handler, uuid, i, cb) {
+  setImmediate(() => {
+    if (i === 32) {
+      return cb(uuid)
+    }
+
+    iterator(handler, uuid + handler(i), ++i, cb)
+  })
+}
+
+/**
+ * @param {number} i
+ * @returns {string}
+ * */
+
+function standardCase (i) {
+  switch (i) {
+    case 12 :
+      return '-4'
+    case 16 :
+      return '-' + (Math.random() * 4 | 8).toString(16)
+    case 8 :
+    case 20 :
+      return '-' + Math.trunc(Math.random() * 16).toString(16)
+  }
+  return Math.trunc(Math.random() * 16).toString(16)
+}
+
+/**
+ * @param {number} i
+ * @returns {string}
+ * */
+
+function clearCase (i) {
+  switch (i) {
+    case 12 :
+      return '4'
+    case 16 :
+      return (Math.random() * 4 | 8).toString(16)
+  }
+  return Math.trunc(Math.random() * 16).toString(16)
 }
--- test/index.js
+++ test/index.js
@@ -1,46 +1,44 @@
 'use strict'
 
 const { strictEqual } = require('assert')
-const { uuid4, uuid4Async, uuid4Clear } = require('../index')
+const { uuid4, uuid4Async, uuid4AsyncClear, uuid4Clear } = require('../index')
 const regexp = new RegExp('^[A-F\\d]{8}-[A-F\\d]{4}-4[A-F\\d]{3}-[89AB][A-F\\d]{3}-[A-F\\d]{12}$', 'i')
 const regexpClear = new RegExp('^[A-F\\d]{12}4[A-F\\d]{3}[89AB][A-F\\d]{15}$', 'i')
 const testSize = 1e5
 
-describe('uuid4 test', function () {
-  it(`expect each time uuid be correct. test size = ${ testSize }`, function () {
-    let x = Date.now()
+describe('uuid v4 generators test', function () {
+
+  it(`uuid4 tests`, function () {
     for (let i = 0; i < testSize; i++) {
       const uuid = uuid4()
-
       strictEqual(regexp.test(uuid), true, `invalid uuid: ${ uuid }`)
       strictEqual(36, uuid.length)
     }
-    const iteration = (Date.now() - x - 2) / testSize
-    console.log(`iteration time: ${ iteration }ms`)
   })
 
-  it(`expect each time uuid be correct. test size = ${ testSize }`, function () {
-    let x = Date.now()
+  it(`uuid4Clear tests`, function () {
     for (let i = 0; i < testSize; i++) {
       const uuid = uuid4Clear()
-
       strictEqual(regexpClear.test(uuid), true, `invalid uuid: ${ uuid }`)
       strictEqual(32, uuid.length)
     }
-    const iteration = (Date.now() - x - 2) / testSize
-    console.log(`iteration time: ${ iteration }ms`)
   })
 
-  it(`expect each time uuid be correct. test size = ${ testSize }`, async function () {
-    this.timeout(3e5)
-    let x = Date.now()
+  it(`uuid4Async tests`, async function () {
     for (let i = 0; i < 100; i++) {
       const uuid = await uuid4Async()
 
       strictEqual(regexp.test(uuid), true, `invalid uuid: ${ uuid }`)
       strictEqual(36, uuid.length)
     }
-    const iteration = (Date.now() - x - 2) / 100
-    console.log(`iteration time: ${ iteration }ms`)
+  })
+
+  it(`uuid4AsyncClear tests`, async function () {
+    for (let i = 0; i < 100; i++) {
+      const uuid = await uuid4AsyncClear()
+
+      strictEqual(regexpClear.test(uuid), true, `invalid uuid: ${ uuid }`)
+      strictEqual(32, uuid.length)
+    }
   })
 })
