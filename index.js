d7f056d04a51f5659d9af69924877bf5d1beb418
fenix <a.sydorenko87@gmail.com>
2019-01-03 20:21:43 +0200

    #feature ([test], index.js, package.json) add uuid4Clear function

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 'use strict'
 
-exports.uuid4 = uuid4
+module.exports = {
+  uuid4,
+  uuid4Clear
+}
 
 function uuid4 () {
   let uuid = ''
 
-  for (let i = 0; i < 32; i++) {
+  let i = 0
+  while (i < 32) {
     switch (i) {
       case 8 :
       case 12 :
@@ -21,6 +25,29 @@ function uuid4 () {
       default:
         uuid += Math.trunc(Math.random() * 16).toString(16)
     }
+    i++
+  }
+
+  return uuid
+}
+
+function uuid4Clear () {
+  let uuid = ''
+
+  let i = 0
+  while (i < 32) {
+    switch (i) {
+      case 8 :
+      case 12 :
+        uuid += '4'
+        break
+      case 16 :
+        uuid += (Math.random() * 4 | 8).toString(16)
+        break
+      default:
+        uuid += Math.trunc(Math.random() * 16).toString(16)
+    }
+    i++
   }
 
   return uuid
--- test/index.js
+++ test/index.js
@@ -1,8 +1,9 @@
 'use strict'
 
-const assert = require('assert')
-const { uuid4 } = require('../index')
+const { strictEqual } = require('assert')
+const { uuid4, uuid4Clear } = require('../index')
 const regexp = new RegExp('^[A-F\\d]{8}-[A-F\\d]{4}-4[A-F\\d]{3}-[89AB][A-F\\d]{3}-[A-F\\d]{12}$', 'i')
+const regexpClear = new RegExp('^[A-F\\d]{12}4[A-F\\d]{3}[89AB][A-F\\d]{15}$', 'i')
 const testSize = 1e5
 
 describe('uuid4 test', function () {
@@ -11,7 +12,20 @@ describe('uuid4 test', function () {
     for (let i = 0; i < testSize; i++) {
       const uuid = uuid4()
 
-      assert.equal(regexp.test(uuid), true, `invalid uuid: ${ uuid }`)
+      strictEqual(regexp.test(uuid), true, `invalid uuid: ${ uuid }`)
+      strictEqual(36, uuid.length)
+    }
+    const iteration = (Date.now() - x - 2) / testSize
+    console.log(`iteration time: ${ iteration }ms`)
+  })
+
+  it(`expect each time uuid be correct. test size = ${ testSize }`, function () {
+    let x = Date.now()
+    for (let i = 0; i < testSize; i++) {
+      const uuid = uuid4Clear()
+
+      strictEqual(regexpClear.test(uuid), true, `invalid uuid: ${ uuid }`)
+      strictEqual(32, uuid.length)
     }
     const iteration = (Date.now() - x - 2) / testSize
     console.log(`iteration time: ${ iteration }ms`)
